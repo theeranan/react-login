cc893d1e281aaa887f21f0ed57d85ebfb1a8a929
Theeranan <theeranan008@gmail.com>
2025-10-01 01:13:02 +0700

    remove unused

--- src/pages/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import Logo from "../assets/img/logo-black.png";
-import { FaBars } from "react-icons/fa";
-import Navbar from "./components/Navbar";
-
-function Navbar() {
-  return <nav className="bg-{#F5F7FA]">Navbar</nav>;
-}
-export default Navbar;
